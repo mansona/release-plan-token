64d8979dd8831edeedff8ed426932180cf18b4cc
Chris Manson <chris@manson.ie>
2026-04-28 22:15:59 +0100

    initial testing commit

---
+++ netlify/edge-functions/token.js
@@ -0,0 +1,40 @@
+import { createAppAuth } from '@octokit/auth-app';
+import { Octokit } from '@octokit/rest';
+
+import { createRemoteJWKSet, jwtVerify } from 'jose';
+
+const JWKS = createRemoteJWKSet(
+  new URL('https://token.actions.githubusercontent.com/.well-known/jwks'),
+);
+
+const appOctokit = new Octokit({
+  authStrategy: createAppAuth,
+  auth: {
+    appId: process.env.APP_ID,
+    privateKey: process.env.PRIVATE_KEY,
+  },
+  baseUrl: 'https://api.github.com',
+});
+
+export default async function getToken(request) {
+  const authHeader = request.headers.get('Authorization');
+  const token = authHeader?.replace('Bearer ', '');
+
+  const { payload } = await jwtVerify(token, JWKS, {
+    issuer: 'https://token.actions.githubusercontent.com',
+    audience: 'release-plan-token',
+  });
+
+  const [owner, repo] = payload.repository.split('/');
+
+  const apps = await appOctokit.apps.getRepoInstallation({
+    owner,
+    repo,
+  });
+
+  console.log(apps);
+
+  return new Response('Hello world');
+}
+
+export const config = { path: '/token' };
